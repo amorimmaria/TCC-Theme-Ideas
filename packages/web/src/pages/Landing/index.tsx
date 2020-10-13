f3475e90f4e253dd2710f51fb977bd6dda72eff9
Jaine Senna <jainesantossenna191@gmail.com>
2020-10-13 13:52:59 -0300

    Melhorando css Landing

--- packages/web/src/pages/Landing/index.tsx
+++ packages/web/src/pages/Landing/index.tsx
@@ -9,36 +9,38 @@ import suggestIcon from '../../assets/icons/lampada.svg'
 import './styles.css'
 
 function Landing() {
-  return (
-    <div id="page-landing">
-      <div id="page-landing-content" className="container">
-        <div id="logo-container">
-          <img src={logoImg} alt="TCC"/>
-          <h2>Sua plaforma de ideias para temas de TCC.</h2>
-          
+    return (
+        <div id="page-menu">
+            <div id="page-menu-top">
+                <div id="page-menu-logo">
+                    <div className="logo-container">
+                        <img src={logoImg} alt="TCC" />
+                        <h2>Sua plataforma de temas para TCC.</h2>
+                    </div>
+
+                    <img src={landingImg} alt="Plataforma de TCCs" className="hero-image" />
+                </div>
+            </div>
+
+            <div id="page-menu-bottom">
+                <div id="welcome-text">
+                    <p>Seja bem-vindo.</p>
+                    <p id="what-todo">O que deseja fazer?</p>
+                </div>
+                <div id="buttons-container">
+                    <a href="/search" className="search">
+                        <img src={searchIcon} alt="Buscar" />
+                        Buscar Tema
+                    </a>
+
+                    <a href="/suggest" className="suggest">
+                        <img src={suggestIcon} alt="Ensinar" />
+                        Sugerir Tema
+                    </a>
+                </div>
+            </div>
         </div>
-
-        <img src={landingImg} alt="Plataforma de estudos" className="hero-image"/>
-
-        <div className="buttons-container">
-          <a href="/search" className="search">
-            <img src={searchIcon} alt="buscar"/>
-            Buscar tema
-          </a>
-
-          <a href="/suggest" className="suggest">
-            <img src={suggestIcon} alt="sugerir"/>
-            Sugerir tema
-          </a>
-        </div>
-        <span className="bem-vindo">
-          <h3>Seja bem-vindo.
-          O que deseja fazer?</h3>
-        </span>
-      </div>
-    </div>
-
-  )
+    )
 }
 
 export default Landing;
