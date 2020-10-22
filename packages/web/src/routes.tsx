6417e7c716ff2761e8e4e10cdf10317b7ddeca5c
Jaine Senna <jainesantossenna191@gmail.com>
2020-10-22 17:19:00 -0300

    Alterando import

--- packages/web/src/routes.tsx
+++ packages/web/src/routes.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import Landing from './pages/Landing';
+import Home from './pages/Home';
 import SearchTheme from './pages/SearchTheme';
 import SuggestTheme from './pages/SuggestTheme';
 
 function Routes(){
   return(
     <BrowserRouter>
-      <Route path="/" exact component={Landing}/>
+      <Route path="/" exact component={Home}/>
       <Route path="/search" component={SearchTheme}/>
       <Route path="/suggest" component={SuggestTheme}/>
 
