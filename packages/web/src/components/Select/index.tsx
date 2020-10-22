f77472a766dab41e9503b80c51ab391012537f1a
amorimmaria <mariadasgracasdiasamorim@gmail.com>
2020-10-22 16:00:55 -0300

    Update na pagina de busca

--- packages/web/src/components/Select/index.tsx
+++ packages/web/src/components/Select/index.tsx
@@ -16,6 +16,7 @@ const Select: React.FC<SelectProps> = ({label, name, options, ...rest}) => {
     <div className="select-block">
       <label htmlFor={name}>{label}</label>
       <select id={name} {...rest}>
+      <option value="" disabled hidden>Escolha uma opção</option>
         {options.map(option => {
           return <option value={option.value}>{option.label}</option>
         })}
--- packages/web/src/pages/SearchTheme/index.tsx
+++ packages/web/src/pages/SearchTheme/index.tsx
@@ -1,7 +1,7 @@
 import React, { FormEvent, useState } from 'react';
 import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
-//import Select from '../../components/Select';
+import Select from '../../components/Select';
 //import api from '../../service/api';
 
 
@@ -36,19 +36,37 @@ function SearchTheme(){
     <div id="page-search" className="container">
       <PageHeader title="Estes são os temas disponíveis.">
         <form  id="search-themes" onSubmit={searchThemes}>
-          <Input
+          <Select
             name="curso"
             label="Curso"
             value={curso}
             onChange= {(e) => {setCurso(e.target.value)}}
+            options={[
+              {value: 'Sistemas de Informação', label:'Sistemas de Informação'},
+              {value: 'Ciência da Computação', label:'Computação de Informação'},
+              {value: 'Ciências e Tecnologias', label:'Ciências de Informação'},
+              {value: 'Design', label:'Design'},
+              {value: 'Engenharia de Computação', label:'Engenharia de Computação'},
+              {value: 'Engenharia de Software', label:'Engenharia de Software'},
+              {value: 'Matemática', label:'Matemática'},
+            ]}
         
           />
-          <Input 
+          <Select 
             name="area"
             label="Área"
             value={area}
             onChange= {(e) => {setArea(e.target.value)}}
- 
+            options={[
+              {value: 'Iot', label:'IoT'},
+              {value: 'Segurança', label:'Segurança'},
+              {value: 'Banco de Dados', label:'Banco de Dados'},
+              {value: 'Desenvolvimento', label:'Desenvolvimento'},
+              {value: 'Engenharia de Software', label:'Engenharia de Software'},
+              {value: 'Inteligência Artificial', label:'Inteligência Artificial'},
+              {value: 'Ciência de Dados', label:'Ciência de Dados'},
+
+            ]}
           />
 
           <button type="submit">
