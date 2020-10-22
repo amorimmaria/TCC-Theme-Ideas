8858a3c2090aed96a914ba53d3091af5af360023
Jaine Senna <jainesantossenna191@gmail.com>
2020-10-22 17:03:35 -0300

    Update

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,15 +1,10 @@
 import express from 'express'
-// import cors from 'cors'
 import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
-
-
-//app.use(cors())
-
 
 app.listen(3333, () => {
   console.log("Server stated on port 3333");
