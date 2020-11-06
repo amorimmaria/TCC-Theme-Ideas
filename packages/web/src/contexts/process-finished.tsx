ae67c0e430b37a0fe8a6e80decfe53fa8df38634
Jaine Senna <jainesantossenna191@gmail.com>
2020-11-06 12:52:14 -0300

    Add pastas contexts e interfaces

---
+++ packages/web/src/contexts/process-finished.tsx
@@ -0,0 +1,39 @@
+import React, { useContext, createContext, useState } from 'react'
+
+interface ProcessFinishedContext {
+    title: string,
+    description: string,
+    defineTitle(newTitle: string): void,
+    defineDescription(newDescription: string): void
+}
+
+const ProcessFinishedContext = createContext({} as ProcessFinishedContext)
+
+export const ProcessFinishedContextProvider: React.FC = ({ children }) => {
+
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+
+    function defineTitle(newTitle: string) {
+        setTitle(newTitle)
+    }
+
+    function defineDescription(newDescription: string) {
+        setDescription(newDescription)
+    }
+
+    return (
+        <ProcessFinishedContext.Provider 
+            value={{ 
+                title, 
+                description, 
+                defineTitle, 
+                defineDescription 
+            }}
+        >{ children }</ProcessFinishedContext.Provider>
+    )
+}
+
+export function useProcessFinished() {
+    return useContext(ProcessFinishedContext)
+}
