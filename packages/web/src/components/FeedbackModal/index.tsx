e126af7f2add1ced59345d2f62c85a58ebeb1e73
amorimmaria <mariadasgracasdiasamorim@gmail.com>
2020-11-06 12:43:20 -0300

    Adicionando pasta FeedbackModal e arquivos index.tsx e styles.css

---
+++ packages/web/src/components/FeedbackModal/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+// Icons
+import { Icon } from '@iconify/react'
+import successIcon from '@iconify/icons-mdi/checkbox-marked-circle-outline'
+import failureIcon from '@iconify/icons-mdi/close-circle-outline'
+
+// CSS styles 
+import './styles.css'
+
+// Interfaces
+interface FeedbackModalProps {
+    status: "success" | "error",
+    message: string,
+    onCloseModal: (...params: any[]) => void
+}
+
+const FeedbackModal: React.FC<FeedbackModalProps> = ({ status, message, onCloseModal }) => {
+    return (
+        <div className="modal-wrapper">
+            <div onClick={onCloseModal} className="modal-backdrop"></div>
+            <div className="feedback-modal">
+                <div className={status}>
+                    <Icon icon={status === "success" ? successIcon : failureIcon} />
+                </div>
+                <p>{message}</p>
+                <button onClick={onCloseModal}>Ok</button>
+            </div>
+        </div>
+    )
+}
+
+export default FeedbackModal
