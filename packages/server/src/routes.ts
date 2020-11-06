ba03c57757b7ec4d5319ce1b409d0056492859fb
amorimmaria <mariadasgracasdiasamorim@gmail.com>
2020-11-06 12:16:47 -0300

    Update routes.ts do server

--- packages/server/src/routes.ts
+++ packages/server/src/routes.ts
@@ -4,7 +4,13 @@ import ThemesControllers from './controllers/ThemesController';
 const routes = express.Router();
 const themesController = new ThemesControllers();
 
+// Usuários
+routes.post("/auth/signup", AuthenticationController.signup)
+routes.post("/auth/signin", AuthenticationController.signin)
 routes.post('/themes', themesController.create);
 routes.get('/themes', themesController.index);
 
+// Conexões
+routes.get("/connections", AuthMiddleware, ConnectionsController.index)
+routes.post("/connections", AuthMiddleware, ConnectionsController.create)
 export default routes;
