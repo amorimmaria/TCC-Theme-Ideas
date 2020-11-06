12ce58d49d256f31b221d5ea4f0d428dd5638a50
Jaine Senna <jainesantossenna191@gmail.com>
2020-11-06 13:02:35 -0300

    User Story US04 - Recuperar Senha #71

---
+++ packages/web/src/pages/Register/ResetPasswordRequest/index.tsx
@@ -0,0 +1,136 @@
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+
+// Icons
+import { Icon } from '@iconify/react'
+import infoIcon from '@iconify/icons-mdi/information-outline'
+
+// Components
+import InputInfo from 'components/InputInfo'
+
+// Images
+import goBackImg from 'assets/images/icons/back.svg'
+
+// Contexts
+import { useAuth } from 'contexts/auth'
+import { useProcessFinished } from 'contexts/process-finished'
+
+// CSS styles
+import './styles.css'
+
+// Interfaces
+import { FormFields } from 'interfaces/forms'
+
+const initialFields = {
+    email: {
+        value: '',
+        validation: /^[a-z-_\d.]{3,}@[a-z]{3,}(\.com|\.br|\.com\.br)$/i,
+        valid: false,
+        info: 'O email precisa estar no formato adequado: exemplo@dominio.com',
+        showInfo: "initial",
+        touched: false
+    }
+}
+
+function ResetPasswordRequest() {
+
+    const history = useHistory()
+    const [fields, setFields] = useState<FormFields>(initialFields as FormFields)
+    const [formValid, setFormValid] = useState(false)
+    const [feedback, setFeedback] = useState('')
+    const authContext = useAuth()
+    const processFinishedContext = useProcessFinished()
+
+    async function sendPasswordResetEmail(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+        if(feedback) setFeedback('')
+
+        const response = await authContext.requestPasswordResetEmail(fields.email.value)
+        if (typeof response === 'string') setFeedback(response)
+        else { 
+            processFinishedContext.defineTitle('Redefinição enviada!')
+            processFinishedContext.defineDescription('Boa, agora é só checar o e-mail que foi enviado para você redefinir sua senha e aproveitar.')
+            history.replace('/auth/recuperar-senha/sucesso') 
+        }
+    }
+
+    function onInputValueChange(e: React.ChangeEvent<HTMLInputElement>) {
+        const inputIdentifier = e.target.id
+        const newInputValue = e.target.value
+
+        const isInputValid = fields[inputIdentifier].validation.test(newInputValue)
+
+        const isFormValid = isInputValid
+
+        if (isFormValid !== formValid)
+            setFormValid(isFormValid)
+
+        if(feedback) setFeedback('')
+
+        setFields({
+            email: {
+                ...fields.email,
+                value: newInputValue,
+                touched: true,
+                valid: isInputValid
+            }
+        })
+    }
+
+    function onInfoHover() {
+        setFields({
+            email: {
+                ...fields.email,
+                showInfo: "show"
+            }
+        })
+    }
+
+    function onInfoLeave() {
+        setFields({
+            email: {
+                ...fields.email,
+                showInfo: "hide"
+            }
+        })
+    }
+
+    function setInputThemes() {
+        return ["input-group", !fields.email.valid && fields.email.touched ? 'invalid' : ''].join(' ')
+    }
+
+    return (
+        <div id="page-register-password-reset-request">
+            <img
+                onClick={() => history.replace('/auth/login')}
+                src={goBackImg}
+                alt="Go back arrow-left"
+            />
+            <h2>Eita, esqueceu sua senha?</h2>
+            <p>Não esquenta, vamos dar um jeito nisso.</p>
+            <form onSubmit={sendPasswordResetEmail}>
+                <div className={setInputThemes()}>
+                    <input
+                        value={fields.email.value}
+                        onChange={onInputValueChange}
+                        type="email"
+                        id="email"
+                        placeholder="E-mail"
+                    />
+                    <div
+                        onMouseEnter={() => onInfoHover()}
+                        onMouseLeave={() => onInfoLeave()}
+                    ><Icon icon={infoIcon} /></div>
+                    <InputInfo show={fields.email.showInfo} info={fields.email.info} />
+                </div>
+                {feedback && <p id="password-reset-request-feedback">{feedback}</p>}
+                <button
+                    type="submit"
+                    disabled={!formValid || authContext.loading}
+                >Enviar</button>
+            </form>
+        </div>
+    )
+}
+
+export default ResetPasswordRequest
---
+++ packages/web/src/pages/Register/UpdateUserPassword/index.tsx
@@ -0,0 +1,198 @@
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+
+// Contexts
+import { useAuth } from 'contexts/auth'
+import { useProcessFinished } from 'contexts/process-finished'
+
+// Components
+import InputInfo from 'components/InputInfo'
+
+// Icons
+import { Icon } from '@iconify/react'
+import showPasswordIcon from '@iconify/icons-mdi/eye'
+import hidePasswordIcon from '@iconify/icons-mdi/eye-off'
+import infoIcon from '@iconify/icons-mdi/information-outline'
+
+// CSS styles
+import './styles.css'
+
+// Interfaces
+import { FormFields } from 'interfaces/forms'
+
+const initialFields = {
+    newPassword: {
+        value: '',
+        validation: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,30}$/,
+        valid: false,
+        info: 'A senha precisa conter entre 8 a 30 caracteres',
+        showInfo: "initial",
+        touched: false
+    },
+    repeatPassword: {
+        value: '',
+        validation: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,30}$/,
+        valid: false,
+        info: '',
+        showInfo: "initial",
+        touched: false
+    }
+}
+
+function UpdateUserPassword() {
+
+    const [showNewPassword, setShowNewPassword] = useState(false)
+    const [showRepeatPassword, setShowRepeatPassword] = useState(false)
+    const [fields, setFields] = useState<FormFields>(initialFields as FormFields)
+    const [formValid, setFormValid] = useState(false)
+    const [feedback, setFeedback] = useState('')
+    const authContext = useAuth()
+    const processFinishedContext = useProcessFinished()
+    const history = useHistory()
+
+    const recoveryToken = history.location.pathname.split('/')[4] 
+
+    function onInputValueChange(e: React.ChangeEvent<HTMLInputElement>) {
+        const inputIdentifier = e.target.id
+        const newInputValue = e.target.value
+        
+        const allFields = Object.keys(fields)
+
+        let isFormValid = true
+        const isInputValid = fields[inputIdentifier].validation.test(newInputValue)
+
+        if (isInputValid) {
+            allFields.forEach(field => {
+                if (isFormValid)
+                    if (field !== inputIdentifier)
+                        isFormValid = fields[field].validation.test(fields[field].value)
+            })
+        } else isFormValid = false
+
+        if (isFormValid) {
+            const validation = new RegExp(`^${inputIdentifier === 'newPassword' ? newInputValue : fields.newPassword.value}$`)
+            
+            isFormValid = validation.test(inputIdentifier === 'repeatPassword' ? newInputValue : fields.repeatPassword.value)
+
+            if (!isFormValid) setFeedback('As senhas são diferentes.')
+            else setFeedback('')
+        }
+
+        if (isFormValid !== formValid)
+            setFormValid(isFormValid)
+
+        setFields({
+            ...fields,
+            [inputIdentifier]: {
+                ...fields[inputIdentifier],
+                value: newInputValue,
+                touched: true,
+                valid: isInputValid
+            }
+        })
+    }
+
+    async function updateUserPassword() {
+        if (feedback) setFeedback('')
+        const updatePasswordData = {
+            token: recoveryToken,
+            new_password: fields.newPassword.value
+        }
+        const response = await authContext.updateUserPassword(updatePasswordData)
+        if (typeof response === 'string') setFeedback(response)
+        else {
+            processFinishedContext.defineTitle('Senha redefinida com sucesso!')
+            processFinishedContext.defineDescription('Agora você já pode logar no TCC Theme Ideas e aproveitar.')
+            history.replace('/auth/recuperar-senha/sucesso')
+        }
+    }
+
+    function onInfoHover() {
+        setFields({
+            ...fields,
+            newPassword: {
+                ...fields.newPassword,
+                showInfo: "show"
+            }
+        })
+    }
+
+    function onInfoLeave() {
+        setFields({
+            ...fields,
+            newPassword: {
+                ...fields.newPassword,
+                showInfo: "hide"
+            }
+        })
+    }
+
+    function setInputThemes(inputIdentifier: string) {
+        return ["input-group", !fields[inputIdentifier].valid && fields[inputIdentifier].touched ? 'invalid' : ''].join(' ')
+    }
+
+    return (
+        <div id="page-register-update-user-password">
+            <h2>Redefinir senha</h2>
+            <p>Preencha o formulário abaixo para redefinir sua senha.</p>
+            <form>
+                <div className={setInputThemes('newPassword')}>
+                    <input
+                        value={fields.newPassword.value}
+                        onChange={onInputValueChange}
+                        maxLength={30}
+                        type={showNewPassword ? "text" : "password"}
+                        id="newPassword"
+                        placeholder="Nova senha"
+                    />
+                    <div className="icon-group">
+                        <div
+                            style={{ cursor: 'pointer' }}
+                            onClick={() => setShowNewPassword(!showNewPassword)}
+                        >
+                            <Icon
+                                color={showNewPassword ? '#8257E5' : '#6A6180'}
+                                icon={showNewPassword ? hidePasswordIcon : showPasswordIcon}
+                            />
+                        </div>
+                        <div
+                            onMouseEnter={() => onInfoHover()}
+                            onMouseLeave={() => onInfoLeave()}
+                        ><Icon icon={infoIcon} /></div>
+                    </div>
+                    <InputInfo show={fields.newPassword.showInfo} info={fields.newPassword.info} />
+                </div>
+
+                <div className={setInputThemes('repeatPassword')}>
+                    <input
+                        value={fields.repeatPassword.value}
+                        onChange={onInputValueChange}
+                        maxLength={30}
+                        type={showRepeatPassword ? "text" : "password"}
+                        id="repeatPassword"
+                        placeholder="Repetir nova senha"
+                    />
+                    <div className="icon-group">
+                        <div
+                            style={{ cursor: 'pointer' }}
+                            onClick={() => setShowRepeatPassword(!showRepeatPassword)}
+                        >
+                            <Icon
+                                color={showRepeatPassword ? '#33AAC2' : '#6A6180'}
+                                icon={showRepeatPassword ? hidePasswordIcon : showPasswordIcon}
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                {feedback && <p id="update-user-password-feedback">{feedback}</p>}
+                <button
+                    disabled={!formValid || authContext.loading}
+                    onClick={updateUserPassword}
+                >Redefinir senha</button>
+            </form>
+        </div>
+    )
+}
+
+export default UpdateUserPassword
