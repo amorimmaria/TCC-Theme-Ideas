a292387043aaff595dd064d8bd96fa70fb31c1a4
amorimmaria <mariadasgracasdiasamorim@gmail.com>
2020-11-06 13:08:04 -0300

    Atualizando o US02 - Buscar Tema #73

---
+++ packages/web/src/components/ThemeItem/index.tsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import axios from 'axios-config'
+
+// Components
+
+// Images
+import whatsappIcon from 'assets/images/icons/whatsapp.svg'
+import noAvatarImg from 'assets/images/sem-avatar.svg'
+
+// CSS styles
+import './styles.css'
+
+
+interface ThemeItemProps {
+    themeId: number,
+    themePhotoURL: string,
+    themeName: string,
+    themeCurso: string,
+    themeArea: string,
+    themeTipoDeUsuario: string,
+    themeSugestaoDeTema: string,
+    themeDescricao: string,
+    themeLinksArtigos: string,
+    themeWhatsapp: number, 
+    themeRef?: any
+}
+
+const ThemeItem: React.FC<ThemeItemProps> = React.memo(props => {
+
+    function createConnection() {
+        axios.post('/connections', { user_id: props.themeId })
+    }
+
+    return (
+        <article className="theme-item" ref={props.themeRef}>
+            <header>
+                <img src={
+                    props.themePhotoURL
+                        ? props.themePhotoURL
+                        : noAvatarImg
+                } alt={props.themeName} />
+                <div>
+                    <strong>{props.themeName}</strong>
+                    <span>{props.themeTipoDeUsuario}</span>
+                </div>
+                <span>
+                    <p><strong>Curso: </strong>{props.themeCurso}</p>
+                    <p><strong>Sugestão de Tema: </strong>{props.themeSugestaoDeTema}</p>
+                    <p><strong>Descrição: </strong>{props.themeDescricao}</p>
+                    <p><strong>Área: </strong>{props.themeArea}</p>
+                    <p><strong>Links de Artigos: </strong>{props.themeLinksArtigos}</p>
+                </span>
+            </header>
+            <footer>
+                
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href={`https://wa.me/${props.themeWhatsapp}`}
+                    onClick={createConnection}
+                >
+                    <img src={whatsappIcon} alt="Ícone do Whatsapp" />
+                    Entrar em contato
+                   
+                </a>
+            </footer>
+        </article>
+    )
+})
+
+export default ThemeItem
--- packages/web/src/pages/SearchTheme/index.tsx
+++ packages/web/src/pages/SearchTheme/index.tsx
@@ -1,83 +1,265 @@
-import React, { FormEvent, useState } from 'react';
-import PageHeader from '../../components/PageHeader';
-import Select from '../../components/Select';
-import ThemesItem, { Themes } from '../../components/ThemesItem';
-import api from '../../service/api';
+import React, { useState, useEffect, useRef, useCallback } from 'react'
+import axios from 'axios-config'
+import { useHistory } from 'react-router-dom'
 
+// Components
+import PageHeader from 'components/PageHeader'
+import ThemeItem from 'components/ThemeItem'
+import Select from 'components/UI/Select'
+import Spinner from 'components/UI/Spinner'
 
-import './styles.css';
+// Contexts
+import { useAuth } from 'contexts/auth'
 
+// Images
+import notFoundIcon from 'assets/images/icons/not-found.svg'
 
-function SearchTheme(){
+// Icons
+import { Icon } from '@iconify/react'
+import searchIcon from '@iconify/icons-mdi/magnify'
 
-  const [themes, setThemes] = useState([]);
- 
-  const [curso, setCurso]= useState('');
-  const [area, setArea]= useState('');
+// CSS styles
+import './styles.css'
 
-  async function searchThemes(e: FormEvent) {
-    e.preventDefault();
-
-    const response = await api.get('themes',{
-      params: {
-        curso,
-        area,
-      }
-    });
-    setThemes(response.data);
-   
-  }
-
-  return (
-    <div id="page-search" className="container">
-      <PageHeader title="Estes são os temas disponíveis.">
-        <form  id="search-themes" onSubmit={searchThemes}>
-          <Select
-            name="curso"
-            label="Curso"
-            value={curso}
-            onChange= {(e) => {setCurso(e.target.value)}}
-            options={[
-              {value: 'Ciência da Computação', label: 'Ciência da Computação'},
-              {value: 'Ciências e Tecnologias', label: 'Ciências e Tecnologias'},
-              {value: 'Design', label: 'Design'},
-              {value: 'Engenharia de Computação', label: 'Engenharia de Computação'},
-              {value: 'Engenharia de Software', label: 'Engenharia de Software'},
-              {value: 'Matemática', label: 'Matemática'},
-              {value: 'Sistemas de Informação', label: 'Sistemas de Informação'},
-            ]}
-        
-          />
-          <Select 
-            name="area"
-            label="Área"
-            value={area}
-            onChange= {(e) => {setArea(e.target.value)}}
-            options={[
-              {value: 'IoT', label: 'IoT'},
-              {value: 'Segurança', label: 'Segurança'},
-              {value: 'Banco de Dados', label: 'Banco de Dados'},
-              {value: 'Desenvolvimento', label: 'Desenvolvimento'},
-              {value: 'Engenharia de Software', label: 'Engenharia de Software'},
-              {value: 'Inteligencia Artificial', label: 'Inteligencia Artificial'},
-              {value: 'Ciencia de Dados', label: 'Ciencia de Dados'},
-
-            ]}
-          />
-
-          <button type="submit">
-           Buscar
-          </button>
-        </form> 
-      </PageHeader>
-
-      <main>
-        {themes.map((themes: Themes) => {
-          return <ThemesItem key={themes.id} themes={themes} />;
-        })}
-      </main>
-    </div>
-  )
+interface ClassItem {
+    id: number,
+    name: 'string',
+    avatar: string,
+    whatsapp: number,
+    sugestaoDeTema: string,
+    tipoDeUsuario: string,
+    curso: string,
+    descricao: string,
+    area: string,
+    linksArtigos: string
 }
 
-export default SearchTheme;
+function SearchTheme() {
+
+    const [classList, setClassList] = useState<ClassItem[]>([])
+    const [loading, setLoading] = useState(false)
+    const [loadingMore, setLoadingMore] = useState(false)
+    const [loadingFeedback, setLoadingFeedback] = useState('')
+    const [reFetch, setReFetch] = useState(true)
+
+    const [pageNumber, setPageNumber] = useState(1)
+    const [hasMore, setHasMore] = useState(true)
+    const observer: any = useRef()
+    const searchMoreNodeRef = useCallback(node => {
+        if (loadingMore) return
+        if (observer.current) observer.current.disconnect()
+        observer.current = new IntersectionObserver(entries => {
+            if (entries[0].isIntersecting && hasMore) {
+                setPageNumber(pageNumber + 1)
+            }
+        })
+        if (node) observer.current.observe(node)
+    }, [loadingMore, hasMore, classList.length]) //eslint-disable-line
+
+    const [curso, setCurso] = useState<string | null>(null)
+    const [tipoDeUsuario, setTipoDeUsuario] = useState<string | null>(null)
+    const [area, setArea] = useState<string | null>(null)
+
+    const history = useHistory()
+    const authContext = useAuth()
+
+    useEffect(() => {
+        (function fetchThemes() {
+            if (reFetch && hasMore) {
+                setReFetch(false)
+                setLoading(true)
+                axios.get('/themes', {
+                    headers: {
+                        authorization: 'Bearer ' + authContext.token
+                    }
+                })
+                    .then(response => {
+                        setLoading(false)
+                        setClassList(response.data.resultsInfo.results)
+                        setHasMore(!!response.data.resultsInfo.next)
+                    })
+                    .catch(() => {
+                        setLoading(false)
+                        alert('Ocorreu um erro desconhecido ao carregar os temas.')
+                        history.replace('/')
+                    })
+            }
+        })()
+    }, [reFetch]) // eslint-disable-line
+
+    useEffect(() => {
+        setLoadingMore(true)
+        axios.get('/themes', {
+            params: {
+                curso,
+                tipoDeUsuario,
+                area,
+                page: pageNumber
+            },
+            headers: {
+                authorization: 'Bearer ' + authContext.token,
+                userid: authContext.user?.__id
+            }
+        })
+            .then(response => {
+                setLoadingMore(false)
+                setClassList([...classList, ...response.data.resultsInfo.results])
+                setHasMore(!!response.data.resultsInfo.next)
+                if(!!!response.data.resultsInfo.next)
+                    setLoadingFeedback('Estes são todos os resultados')
+            })
+            .catch(() => {
+                setLoadingMore(false)
+                setLoadingFeedback('Erro ao buscar mais temas. Tente novamente mais tarde.')
+            })
+    }, [pageNumber]) // eslint-disable-line
+
+    function filterThemes(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+        setLoading(true)
+        setPageNumber(1)
+        setClassList([])
+        axios.get('/themes', {
+            params: {
+                curso,
+                tipoDeUsuario,
+                area
+            },
+            headers: {
+                authorization: 'Bearer ' + authContext.token,
+                userid: authContext.user?.__id
+            }
+        })
+            .then(response => {
+                setLoading(false)
+                setClassList([...response.data.resultsInfo.results])
+                setHasMore(!!response.data.resultsInfo.next)
+            })
+            .catch(() => {
+                setLoading(false)
+                alert('Erro ao aplicar filtros. Mostrando todos os resultados.')
+                setCurso(null)
+                setTipoDeUsuario(null)
+                setArea(null)
+                setReFetch(true)
+            })
+    }
+
+    return (
+        <div id="page-theme-list" className="container">
+            <PageHeader
+                title="Estes são os temas disponíveis."
+            >
+                <form id="search-themes" onSubmit={filterThemes}>
+                    <Select
+                        selectLabel="Curso"
+                        selected={{ value: "", label: "Todos os cursos" }}
+                        items={[
+                            {value: "", label: "Todos os cursos" },
+                            {value: 'Ciência da Computação', label: 'Ciência da Computação'},
+                            {value: 'Ciências e Tecnologias', label: 'Ciências e Tecnologias'},
+                            {value: 'Design', label: 'Design'},
+                            {value: 'Engenharia de Computação', label: 'Engenharia de Computação'},
+                            {value: 'Engenharia de Software', label: 'Engenharia de Software'},
+                            {value: 'Matemática', label: 'Matemática'},
+                            {value: 'Sistemas de Informação', label: 'Sistemas de Informação'},
+                          ]}
+                        onOptionSelect={selected => setCurso(selected.value)}
+                    />
+                    <Select
+                        selectLabel="Tipo de usuário"
+                        selected={{ value: "", label: "Todos os usuários" }}
+                        items={[
+                            {value: "", label: "Todos os usuários"},
+                            {value: 'Docente', label: 'Docente'},
+                            {value: 'Discente', label: 'Discente'}
+                          ]}
+                        onOptionSelect={selected => setTipoDeUsuario(selected.value)}
+                    />
+                    <Select
+                        selectLabel="Área"
+                        selected={{ value: "", label: "Todas as áreas" }}
+                        items={[
+                            {value: "", label: "Todas as áreas"},
+                            {value: 'IoT', label: 'IoT'},
+                            {value: 'Segurança', label: 'Segurança'},
+                            {value: 'Banco de Dados', label: 'Banco de Dados'},
+                            {value: 'Desenvolvimento', label: 'Desenvolvimento'},
+                            {value: 'Engenharia de Software', label: 'Engenharia de Software'},
+                            {value: 'Inteligencia Artificial', label: 'Inteligencia Artificial'},
+                            {value: 'Ciencia de Dados', label: 'Ciencia de Dados'},
+                          ]}
+                        onOptionSelect={selected => setArea(selected.value)}
+                    />
+                    <button type="submit">
+                        <Icon icon={searchIcon} />
+                        Buscar
+                    </button>
+                </form>
+            </PageHeader>
+
+            <main>
+                {
+                    loading
+                        ? <div className="spinner-resizer"><Spinner /></div>
+                        : classList.length > 0
+                            ? (
+                                <>
+                                    {classList.map((currentClass, index) => {
+                                        if (index === classList.length - 4)
+                                            return (
+                                                <ThemeItem
+                                                    key={index}
+                                                    themeRef={searchMoreNodeRef}
+                                                    themeId={currentClass.id}
+                                                    themePhotoURL={currentClass.avatar}
+                                                    themeName={currentClass.name}
+                                                    themeCurso={currentClass.curso}
+                                                    themeDescricao={currentClass.descricao}
+                                                    themeArea={currentClass.area}
+                                                    themeSugestaoDeTema={currentClass.sugestaoDeTema}
+                                                    themeTipoDeUsuario={currentClass.tipoDeUsuario}
+                                                    themeLinksArtigos={currentClass.linksArtigos}
+                                                    themeWhatsapp={currentClass.whatsapp}
+                                                />
+                                            )
+                                        return (
+                                            <ThemeItem
+                                                key={index}
+                                                themeId={currentClass.id}
+                                                themePhotoURL={currentClass.avatar}
+                                                themeName={currentClass.name}
+                                                themeCurso={currentClass.curso}
+                                                themeDescricao={currentClass.descricao}
+                                                themeArea={currentClass.area}
+                                                themeSugestaoDeTema={currentClass.sugestaoDeTema}
+                                                themeTipoDeUsuario={currentClass.tipoDeUsuario}
+                                                themeLinksArtigos={currentClass.linksArtigos}
+                                                themeWhatsapp={currentClass.whatsapp}
+                                            />
+                                        )
+                                    })}
+                                    {
+                                        hasMore
+                                            ? loadingMore && <div className="spinner-resizer"><Spinner /></div>
+                                            : <p id="all-results">{loadingFeedback}</p>
+                                    }
+                                </>
+                            )
+                            : (
+                                <section className="no-themes-found">
+                                    <header>
+                                        Oops! Parece que não foi encontrado nenhum tema <br />
+                                        disponível. Tente alterar os filtros.
+                                    </header>
+
+                                    <img src={notFoundIcon} alt="Nenhum tema foi encontrado" />
+                                </section>
+                            )
+                }
+            </main>
+        </div>
+    )
+}
+
+export default SearchTheme
