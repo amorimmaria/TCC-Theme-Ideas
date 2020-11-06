21088cd03039a9d3c0cd4cf1f83332bd89037016
Jaine Senna <jainesantossenna191@gmail.com>
2020-11-06 11:50:59 -0300

    Update create theme e favoritos

--- packages/server/src/database/migrations/01_create_themes.ts
+++ packages/server/src/database/migrations/01_create_themes.ts
@@ -1,29 +1,24 @@
-import Knex from 'knex';
+import Knex from "knex"
 
-export async function up(knex:Knex) {
-  return knex.schema.createTable('themes', table =>{
-    table.increments('id').primary();
-    table.string('name').notNullable();
-    table.string('avatar').notNullable();
-    table.string('tipoDeUsuario').notNullable();
-    table.string('curso').notNullable();
-    table.string('sugestaoDeTema').notNullable();
-    table.string('descricao').notNullable();
-    table.string('area').notNullable();
-    table.string('linksArtigos').notNullable();
-    
-    table.integer('user_id')
-      .notNullable()
-      .references('id')
-      .inTable('users')
-      .onUpdate('CASCADE')
-      .onDelete('CASCADE');
-
-  });
-  
+export async function up(knex: Knex) {
+    await knex.schema.createTable("themes", table => {
+        table.increments()
+        table.string('tipoDeUsuario').notNullable();
+        table.string('curso').notNullable();
+        table.string('sugestaoDeTema').notNullable();
+        table.string('descricao').notNullable();
+        table.string('area').notNullable();
+        table.string('linksArtigos').notNullable();
+        
+        table.string("__user_id")
+            .notNullable()
+            .references("__id")
+            .inTable("users")
+            .onDelete("CASCADE")
+            .onUpdate("CASCADE")
+    })
 }
 
-export async function down(knex:Knex) {
-  return knex.schema.dropTable('themes');
-  
+export async function down(knex: Knex) {
+    await knex.schema.dropTable("themes")
 }
---
+++ packages/server/src/database/migrations/04_create_favourites.ts
@@ -0,0 +1,25 @@
+import Knex from "knex"
+
+export async function up(knex: Knex) {
+    await knex.schema.createTable("favourites", table => {
+        table.integer("user_id")
+            .notNullable()
+            .references("__id")
+            .inTable("users")
+            .onDelete("CASCADE")
+            .onUpdate("CASCADE")
+        
+        table.integer("favourite_id")
+            .notNullable()
+            .references("__id")
+            .inTable("users")
+            .onDelete("CASCADE")
+            .onUpdate("CASCADE")
+            
+        table.string("token")
+    })
+}
+
+export async function down(knex: Knex) {
+    await knex.schema.dropTable("favourites")
+}
