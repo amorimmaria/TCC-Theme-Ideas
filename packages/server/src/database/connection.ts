23cb6c0c1e6a85f70c664ec936010758d0737d56
Jaine Senna <jainesantossenna191@gmail.com>
2020-11-06 11:53:08 -0300

    Update connections.ts

--- packages/server/src/database/connection.ts
+++ packages/server/src/database/connection.ts
@@ -1,13 +1,8 @@
-import knex from 'knex';
-import path from 'path';
+import knex from "knex"
+import path from "path"
 
-const db = knex({
-  client: 'sqlite3',
-  connection: {
-    filename: path.resolve(__dirname, 'database.sqlite')
-  },
-  useNullAsDefault: true,
-
-});
-
-export default db;
+export default knex({
+    client: "sqlite3",
+    connection: { filename: path.resolve(__dirname, "database.sqlite") },
+    useNullAsDefault: true
+})
