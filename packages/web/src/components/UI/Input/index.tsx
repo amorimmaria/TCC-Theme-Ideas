b025c35ef4d984d066e0689cc638051a400b3582
amorimmaria <mariadasgracasdiasamorim@gmail.com>
2020-11-06 12:46:42 -0300

    Adicionando pasta de inputs e selects

---
+++ packages/web/src/components/UI/Input/index.tsx
@@ -0,0 +1,178 @@
+import React, { InputHTMLAttributes, useState } from 'react'
+
+// Utils
+import { formatCurrentPhone } from 'utils/format'
+
+// Components
+import InputInfo from 'components/InputInfo'
+
+// Icons
+import { Icon } from '@iconify/react'
+import showPasswordIcon from '@iconify/icons-mdi/eye'
+import hidePasswordIcon from '@iconify/icons-mdi/eye-off'
+import infoIcon from '@iconify/icons-mdi/information-outline'
+
+// CSS styles
+import './styles.css'
+
+// Interfaces
+import { FormFields } from 'interfaces/forms'
+
+type InputTextarea = InputHTMLAttributes<HTMLInputElement> & InputHTMLAttributes<HTMLTextAreaElement>
+
+interface InputProps extends InputTextarea {
+    inputType: "input" | "textarea",
+    inputContentType: string,
+    inputLabel: string,
+    inputId: string,
+    fields?: FormFields,
+    setFields?: (value: React.SetStateAction<FormFields>) => void,
+    formValid?: boolean,
+    setFormValid?: (value: React.SetStateAction<boolean>) => void,
+    hasInfo?: boolean,
+    feedback?: string,
+    setFeedback?: (value: React.SetStateAction<string>) => void,
+}
+
+const Input: React.FC<InputProps> = ({
+    inputType,
+    inputContentType,
+    inputLabel,
+    inputId,
+    fields,
+    setFields,
+    formValid,
+    setFormValid,
+    hasInfo,
+    feedback,
+    setFeedback,
+    ...inputProps
+}) => {
+
+    const [showPassword, setShowPassword] = useState(false)
+
+    function onInputValueChange(e: React.ChangeEvent<any>) {
+        const inputIdentifier = e.target.id
+        let newInputValue: string = e.target.value
+        
+        if (inputIdentifier === "whatsapp") 
+            newInputValue = formatCurrentPhone(newInputValue)
+
+        const allFields = Object.keys(fields!)
+
+        let isFormValid = true
+        const isInputValid = fields![inputIdentifier].validation.test(newInputValue)
+
+        if (isInputValid) {
+            allFields.forEach(field => {
+                if (isFormValid)
+                    if (field !== inputIdentifier)
+                        isFormValid = fields![field].validation.test(fields![field].value)
+            })
+        } else isFormValid = false
+
+        if (isFormValid !== formValid)
+            setFormValid!(isFormValid)
+
+        if (feedback) setFeedback!('')
+
+        setFields!({
+            ...fields,
+            [inputIdentifier]: {
+                ...fields![inputIdentifier],
+                value: newInputValue,
+                touched: true,
+                valid: isInputValid
+            }
+        })
+    }
+
+    function onInfoHover() {
+        setFields!({
+            ...fields,
+            [inputId]: {
+                ...fields![inputId],
+                showInfo: "show"
+            }
+        })
+    }
+
+    function onInfoLeave() {
+        setFields!({
+            ...fields,
+            [inputId]: {
+                ...fields![inputId],
+                showInfo: "hide"
+            }
+        })
+    }
+
+    function setInputThemes() {
+        return [
+            "input-group",
+            fields ?
+                !fields[inputId].valid && fields[inputId].touched ? 'invalid' : ''
+                : ''
+        ].join(' ')
+    }
+
+    return (
+        <div id="input-wrapper">
+            <label htmlFor={inputId}>{inputLabel}</label>
+            <div className={setInputThemes()}>
+                {
+                    inputType === "input"
+                        ? (
+                            <input
+                                value={fields ? fields[inputId].value : inputProps.value}
+                                onChange={onInputValueChange}
+                                id={inputId}
+                                type={inputContentType}
+                                {...inputProps}
+                            />
+                        ) : (
+                            <textarea
+                                onChange={onInputValueChange}
+                                {...inputProps}
+                                id={inputId}
+                            />
+                        )
+                }
+                {
+                    inputContentType !== "password"
+                        ? hasInfo && (
+                            <div
+                                onMouseEnter={onInfoHover}
+                                onMouseLeave={onInfoLeave}
+                            ><Icon icon={infoIcon} /></div>
+                        ) : (
+                            <div className="icon-group">
+                                <div
+                                    style={{ cursor: 'pointer' }}
+                                    onClick={() => setShowPassword(!showPassword)}
+                                >
+                                    <Icon
+                                        color={showPassword ? '#33AAC2' : '#33AAC2'}
+                                        icon={showPassword ? hidePasswordIcon : showPasswordIcon}
+                                    />
+                                </div>
+                                <div
+                                    onMouseEnter={onInfoHover}
+                                    onMouseLeave={onInfoLeave}
+                                ><Icon icon={infoIcon} /></div>
+                            </div>
+                        )
+                }
+                {
+                    hasInfo &&
+                    <InputInfo
+                        show={fields![inputId].showInfo}
+                        info={fields![inputId].info}
+                    />
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Input
---
+++ packages/web/src/components/UI/Select/index.tsx
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react'
+
+// Icons
+import { Icon } from '@iconify/react'
+import caretDownIcon from '@iconify/icons-mdi/caret-down'
+
+// CSS styles
+import './styles.css'
+
+interface SelectItem {
+    value: string,
+    label: string
+}
+
+interface SelectProps {
+    selectLabel: string,
+    selected: SelectItem,
+    items: SelectItem[],
+    onOptionSelect: (opt: SelectItem) => void
+}
+
+const Select: React.FC<SelectProps> = ({ selectLabel, selected, items, onOptionSelect }) => {
+    const [themes, setThemes] = useState(["Select", "Close"])
+    const [currentSelected, setCurrentSelected] = useState({ value: selected.value, label: selected.label })
+
+    function toggleSelect() {
+        document.querySelectorAll('div.Select.Open')
+            .forEach(el => {
+                el.classList.remove('Open')
+                el.classList.add('Close')
+            })
+        if (items.length !== 0) {
+            let newThemes = []
+            if (themes.includes("Open"))
+                newThemes = ["Select", "Close"]
+            else {
+                newThemes = ["Select", "Open"]
+            }
+            setThemes(newThemes)
+        }
+    }
+
+    function setSelection(item: SelectItem) {
+        const newThemes = [...themes]
+        newThemes.pop()
+        newThemes.push("Close")
+        setThemes(newThemes)
+        setCurrentSelected(item)
+    }
+
+    useEffect(() => {
+        onOptionSelect(currentSelected)
+    }, [currentSelected]) //eslint-disable-line
+
+    return (
+        <div className={themes.join(' ')}>
+            <label>{selectLabel}</label>
+            <div onClick={toggleSelect} className="SelectContent">
+                <div className="SelectSelector">
+                    <span>{currentSelected.label}</span>
+                    {items.length !== 0 && <Icon icon={caretDownIcon} />}
+                </div>
+                <ul>{
+                    items.map((item, i) => (
+                        <li
+                            className={item.value === currentSelected.value ? 'Selected' : ''}
+                            key={i}
+                            onClick={() => setSelection(item)}
+                        >{item.label}</li>
+                    ))
+                }</ul>
+            </div>
+        </div>
+    )
+}
+
+export default Select
