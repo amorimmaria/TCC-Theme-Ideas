d373047a85520fce9cfd874a43d5d53654dee10f
Jaine Senna <jainesantossenna191@gmail.com>
2020-11-06 11:38:11 -0300

    Update server.ts

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,12 +1,14 @@
-import express from 'express'
-import cors from 'cors';
-import routes from './routes';
+import path from "path"
+import dotenv from "dotenv"
+dotenv.config({path: path.resolve(__dirname, ".env")})
+import express from "express"
+import bodyParser from 'body-parser'
+import routes from "./routes"
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(routes);
+const app = express()
+const PORT = 3333
 
-app.listen(3333, () => {
-  console.log("Server stated on port 3333");
-});
+app.use(bodyParser.json({limit: '1000kb'}))
+app.use(routes)
+
+app.listen(PORT, () => console.log("Server started @PORT=" + PORT))
