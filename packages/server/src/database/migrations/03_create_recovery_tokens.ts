793419621f4574609a5b194ac3da8b33cf876f66
amorimmaria <mariadasgracasdiasamorim@gmail.com>
2020-11-06 12:08:49 -0300

    Adicionando migrations

---
+++ packages/server/src/database/migrations/03_create_recovery_tokens.ts
@@ -0,0 +1,19 @@
+import Knex from "knex"
+
+export async function up(knex: Knex) {
+    await knex.schema.createTable("recovery_tokens", table => {
+        table.increments()
+        table.integer("user_id")
+            .notNullable()
+            .references("__id")
+            .inTable("users")
+            .onDelete("CASCADE")
+            .onUpdate("CASCADE")
+            
+        table.string("token")
+    })
+}
+
+export async function down(knex: Knex) {
+    await knex.schema.dropTable("recovery_tokens")
+}
