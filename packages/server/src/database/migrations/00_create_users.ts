cec34cd29e93a298a356ce620b92e965d3f2e5c5
amorimmaria <mariadasgracasdiasamorim@gmail.com>
2020-11-06 12:07:45 -0300

    Update migration de user

--- packages/server/src/database/migrations/00_create_users.ts
+++ packages/server/src/database/migrations/00_create_users.ts
@@ -1,14 +1,16 @@
-import Knex from 'knex';
+import Knex from "knex"
 
-export async function up(knex:Knex) {
-  return knex.schema.createTable('users', table =>{
-    table.increments('id').primary();
-    table.string('name').notNullable();
-  });
-  
+export async function up(knex: Knex) {
+    await knex.schema.createTable("users", table => {
+        table.string("__id").unique().primary()
+        table.string("name").notNullable()
+        table.string("email").notNullable()
+        table.string("password").notNullable()
+        table.binary("avatar")
+        table.string("whatsapp")
+    })
 }
 
-export async function down(knex:Knex) {
-  return knex.schema.dropTable('users');
-  
+export async function down(knex: Knex) {
+    await knex.schema.dropTable("users")
 }
