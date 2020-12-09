45bbee75d39360b9a1ff81ffa5d647c742cb1784
amorimmaria <mariadasgracasdiasamorim@gmail.com>
2020-12-09 10:32:35 -0300

    Corrigindo code smell do SuggestTheme #89

--- packages/web/src/pages/SuggestTheme/index.tsx
+++ packages/web/src/pages/SuggestTheme/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Redirect, useHistory } from 'react-router-dom'
+import {  useHistory } from 'react-router-dom'
 import axios from '../../axios-config'
 
 
